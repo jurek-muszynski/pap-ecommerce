efe1070591f873a6f181189736764dbfccd2f77e
jantas <01187205@pw.edu.pl>
2024-11-28 22:20:44 +0100

    feat(OrderItem): Added OrderItem classes, base

--- backend/src/main/java/pap/backend/orderItem/OrderItem.java
+++ backend/src/main/java/pap/backend/orderItem/OrderItem.java
@@ -1,4 +1,70 @@
 package pap.backend.orderItem;
 
+import jakarta.persistence.*;
+import pap.backend.order.Order;
+import pap.backend.product.Product;
+
+@Entity
+@Table
 public class OrderItem {
+
+    @Id
+    @SequenceGenerator(
+            name = "orderItem_sequence",
+            sequenceName = "orderItem_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "orderItem_sequence"
+    )
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    public OrderItem() {}
+
+    public OrderItem(Product product, Order order) {
+        this.product = product;
+        this.order = order;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "id=" + id +
+                ", product=" + product +
+                ", order=" + order +
+                '}';
+    }
 }
