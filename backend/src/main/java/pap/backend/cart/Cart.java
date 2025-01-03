d90e1710ce103baafd18e9257f42f37e46739de3
jantas <01187205@pw.edu.pl>
2025-01-03 13:30:42 +0100

    feat(cart): Added Cart

--- backend/src/main/java/pap/backend/cart/Cart.java
+++ backend/src/main/java/pap/backend/cart/Cart.java
@@ -1,4 +1,70 @@
 package pap.backend.cart;
 
+import jakarta.persistence.*;
+import pap.backend.user.User;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "carts")
 public class Cart {
+
+    @Id
+    @SequenceGenerator(
+            name = "cart_sequence",
+            sequenceName = "cart_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "cart_sequence"
+    )
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "last_update", nullable = false)
+    private LocalDate lastUpdate;
+
+    public Cart() {}
+
+    public Cart(User user, LocalDate lastUpdate) {
+        this.user = user;
+        this.lastUpdate = lastUpdate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDate lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "id=" + id +
+                ", user=" + user +
+                ", lastUpdate=" + lastUpdate +
+                '}';
+    }
 }
