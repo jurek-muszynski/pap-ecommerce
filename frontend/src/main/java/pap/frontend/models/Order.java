1cbf00aa7a6ca4b34e00adb053d0a95262d899c4
jantas <01187205@pw.edu.pl>
2025-01-07 23:04:45 +0100

    feat(models): Added missing models in frontend

---
+++ frontend/src/main/java/pap/frontend/models/Order.java
@@ -0,0 +1,43 @@
+package pap.frontend.models;
+
+import java.time.LocalDate;
+
+public class Order {
+
+    private Long id;
+    private User user;
+    private LocalDate date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", user=" + user +
+                ", date=" + date +
+                '}';
+    }
+}
---
+++ frontend/src/main/java/pap/frontend/models/Review.java
@@ -0,0 +1,61 @@
+package pap.frontend.models;
+
+public class Review {
+
+    private Long id;
+    private User user;
+    private Product product;
+    private Integer rate;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getRate() {
+        return rate;
+    }
+
+    public void setRate(Integer rate) {
+        this.rate = rate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", user=" + user +
+                ", product=" + product +
+                ", rate=" + rate +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
