8ffc549687e20524a59a334c65681e3679147640
jmuszyns <j.muszynski@stud.elka.pw.edu.pl>
2025-01-08 16:40:39 +0100

    feat(auth): Auth initialized, User MVC refactor

---
+++ backend/src/main/java/pap/backend/auth/AuthResponse.java
@@ -0,0 +1,15 @@
+package pap.backend.auth;
+
+
+public class AuthResponse {
+    private String token;
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+}
---
+++ backend/src/main/java/pap/backend/user/UserRole.java
@@ -0,0 +1,5 @@
+package pap.backend.user;
+
+public enum UserRole {
+    ADMIN, USER
+}
